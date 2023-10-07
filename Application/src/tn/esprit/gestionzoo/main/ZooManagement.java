72675d252c673985e466e302def025b545f4a9bc
YosriElhadj <yosri.elhadj@esprit.tn>
2023-10-07 15:55:04 +0100

    prosit4_2

--- Application/src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ Application/src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -1,3 +1,7 @@
+package tn.esprit.gestionzoo.main;
+import tn.esprit.gestionzoo.entities.Animal;
+import tn.esprit.gestionzoo.entities.Zoo;
+
 public class ZooManagement {
 
     public static void main(String[] args) {
